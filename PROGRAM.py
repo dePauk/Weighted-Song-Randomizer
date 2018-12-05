ed30fc2f9c4b615cf9b9b2b7055c668c4d65b067
Darjan Pavšič <darjan.pavsic@gmail.com>
2018-12-05 19:24:34 +0100

    Defined class

--- PROGRAM.py
+++ PROGRAM.py
@@ -1,4 +1,19 @@
 import random
 import tkinter
 
-pass
+
+
+xls = open('sample-songlist.xlsx')
+
+class Playlist:
+    def __init__ (self):
+        playlist_length = 10
+    def __repr__ (self):
+        return ("A Playlist")
+
+testpl = Playlist()
+
+
+
+
+
