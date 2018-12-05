ac654953a13f1dda331ee892f89172e28212e4d1
Darjan Pavšič <darjan.pavsic@gmail.com>
2018-12-05 20:58:52 +0100

    Class defs

--- PROGRAM.py
+++ PROGRAM.py
@@ -2,22 +2,31 @@ import random
 import tkinter as tk
 
 
-
 xls = open('sample-songlist.xlsx')
 
 class Playlist:
+
     def __init__ (self):
-        playlist_length = 10
+        self.length = 10
+        self.name = "TB named"
+        pass
     def __repr__ (self):
-        return ("A Playlist")
+        return ("Playlist:" + " " +(self.name))
 
     def set_length(self,x):
-        playlist_length = x
+        self.length = x
         
+    def rename(self,x):
+        self.name = str(x)
+    
+
+
 
 testpl = Playlist()
 
-okno = tk.Tk()
+
+
+#okno = tk.Tk()
 
 
 
