0deb1d74a92e233250860fd6def07219061c0c12
Darjan Pavšič <darjan.pavsic@gmail.com>
2018-12-06 16:10:25 +0100

    A new function added

--- PROGRAM.py
+++ PROGRAM.py
@@ -9,21 +9,26 @@ class Playlist:
     def __init__ (self):
         self.length = 10
         self.name = "TB named"
-        pass
+        self.songs = set()
+        #self.songs = {"song1","song2","song3","song4","song5",
+        #              "song6","song7","song8","song9","song10"}
+
     def __repr__ (self):
         return ("Playlist:" + " " +(self.name))
 
     def set_length(self,x):
         self.length = x
-        
+        self.songs = set()
+        for i in range(0,x):
+            self.songs.add(random.random())# Instead of a song set currently just a random number set
+            
+
     def rename(self,x):
         self.name = str(x)
     
 
 
-
 testpl = Playlist()
-
 
 
 #okno = tk.Tk()
